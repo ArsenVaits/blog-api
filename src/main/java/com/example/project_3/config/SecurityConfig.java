74aff1696e70f2fbd713a7448589660727b5d1db
Arsen Vaits <arsenvaits@gmail.com>
2026-08-11 19:29:08 +0500

    Коректеровка мелких ошибок

--- src/main/java/com/example/project_3/config/SecurityConfig.java
+++ src/main/java/com/example/project_3/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.example.project_3.security.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -32,6 +33,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated()
